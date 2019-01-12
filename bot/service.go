d2c976354fbfb5d771e2e02237fe92403dc351da
indes <wl@live.it>
2019-01-12 16:08:53 +0800

    Add content insert
    New source's contents get and insert

--- bot/service.go
+++ bot/service.go
@@ -2,15 +2,12 @@ package bot
 
 import (
 	"fmt"
-	"github.com/indes/go-rssbot/bot/model"
-	_ "github.com/indes/go-rssbot/bot/model"
+	"github.com/indes/go-rssbot/model"
 	"gopkg.in/tucnak/telebot.v2"
-	"log"
 )
 
 func registFeed(chat *telebot.Chat, url string) {
 	msg, _ := B.Send(chat, "处理中...")
-	log.Println(msg)
 
 	k, _ := model.FindOrNewSourceByUrl(url)
 	err := model.RegistFeed(chat.ID, k.ID)
