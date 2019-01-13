c45be1508b5ed4a2992266ef5ce3f36a1cb52092
indes <wl@live.it>
2019-01-13 11:16:57 +0800

    Update telegraph publish

--- tgraph/auth.go
+++ tgraph/auth.go
@@ -1,8 +1,8 @@
 package tgraph
 
 const (
-	authToken = "77d1ae436c753a81300ce36b0ffcee22fd304616c4e013524c45d598c39e"
-	authorUrl = "https://github.com/indes/rssflow"
+	authToken  = "77d1ae436c753a81300ce36b0ffcee22fd304616c4e013524c45d598c39e"
+	authorUrl  = "https://github.com/indes/rssflow"
 	authorName = "RSS"
-	verbose    = true
+	verbose    = false
 )
