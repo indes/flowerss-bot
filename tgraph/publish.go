8a12b33eeaac8522555422dad95585c82ca54be4
indes <wl@live.it>
2019-01-16 20:48:50 +0800

    feat(publish): modify telegraph publish template and add copyright

--- tgraph/publish.go
+++ tgraph/publish.go
@@ -1,16 +1,27 @@
 package tgraph
 
 import (
+	"fmt"
 	"log"
 )
 
-func PublishItem(title string, html string) string {
-	url, _ := PublishHtml(title, html)
-	return url
-}
+func PublishHtml(sourceTitle string, title string, rawLink string, html string) (string, error) {
+	//html = fmt.Sprintf(
+	//	"<p>本文章由 <a href=\"https://github.com/indes/rssflow\">RSSFlow</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><hr>",
+	//) + html + fmt.Sprintf(
+	//	"<hr><p>本文章由 <a href=\"https://github.com/indes/rssflow\">RSSFlow</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+	//	rawLink,
+	//	title,
+	//	sourceTitle,
+	//)
 
-func PublishHtml(title string, html string) (string, error) {
-	if page, err := client.CreatePageWithHTML(title, authorName, authorUrl, html, true); err == nil {
+	html = html + fmt.Sprintf(
+		"<hr><p>本文章由 <a href=\"https://github.com/indes/rssflow\">RSSFlow</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+		rawLink,
+		title,
+		sourceTitle,
+	)
+	if page, err := client.CreatePageWithHTML(title+" - "+sourceTitle, authorName, authorUrl, html, true); err == nil {
 		log.Printf("Created telegraph page url: %s", page.URL)
 		return page.URL, err
 	} else {
