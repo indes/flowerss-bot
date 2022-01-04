cf3cc0bd607542ce90b96603c267d55b8b13ca0c
KevinChen <67411136+makubex2010@users.noreply.github.com>
2022-01-04 19:23:53 +0800

    Update publish.go

--- internal/tgraph/publish.go
+++ internal/tgraph/publish.go
@@ -11,16 +11,16 @@ import (
 
 func PublishHtml(sourceTitle string, title string, rawLink string, htmlContent string) (string, error) {
 	//html = fmt.Sprintf(
-	//	"<p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><hr>",
+	//	"<p>本文章由 <a href=\"https://t.me/freeinformation_bot\">無料案內所</a> 抓取，版權歸<a href=\"\">原本網站</a>所有。</p><hr>",
 	//) + html + fmt.Sprintf(
-	//	"<hr><p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+	//	"<hr><p>本文章由 <a href=\"https://t.me/freeinformation_bot\">無料案內所</a> 抓取，版權歸<a href=\"\">原本網站</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
 	//	rawLink,
 	//	title,
 	//	sourceTitle,
 	//)
 
 	htmlContent = html.UnescapeString(htmlContent) + fmt.Sprintf(
-		"<hr><p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+		"<hr><p>本文章由 <a href=\"https://t.me/freeinformation_bot\">無料案內所</a> 抓取，版權歸<a href=\"\">原本網站</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
 		rawLink,
 		title,
 		sourceTitle,
