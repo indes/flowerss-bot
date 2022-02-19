080169dec4ad7ebf3f036b1870096292c9d6f72d
indes <wl@live.it>
2022-02-19 21:45:02 +0800

    feat: migrate export command

--- internal/bot/handler/export.go
+++ internal/bot/handler/export.go
@@ -1,70 +1,101 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/indes/flowerss-bot/internal/bot/message"
+	"github.com/indes/flowerss-bot/internal/model"
+	"github.com/indes/flowerss-bot/internal/opml"
+	"go.uber.org/zap"
 	tb "gopkg.in/telebot.v3"
 )
 
 type Export struct {
 }
 
-func (e *Export) Handle(ctx tb.Context) {
-	//mention := message.MentionFromMessage(ctx.Message())
-	//var sourceList []model.Source
-	//var err error
-	//if mention == "" {
-	//	sourceList, err = model.GetSourcesByUserID(ctx.Chat().ID)
-	//	if err != nil {
-	//		zap.S().Warnf(err.Error())
-	//		ctx.Send("导出失败")
-	//		return
-	//	}
-	//} else {
-	//	channelChat, err := ctx.Bot().ChatByID(mention)
-	//	if err != nil {
-	//		ctx.Send("导出失败")
-	//		return
-	//	}
-	//
-	//	adminList, err := ctx.Bot().AdminsOf(channelChat)
-	//	if err != nil {
-	//		ctx.Send("导出失败")
-	//		return
-	//	}
-	//
-	//	senderIsAdmin := false
-	//	for _, admin := range adminList {
-	//		if ctx.ChatMember().Sender.ID == admin.User.ID {
-	//			senderIsAdmin = true
-	//		}
-	//	}
-	//
-	//	if !senderIsAdmin {
-	//		ctx.Send(fmt.Sprintf("非频道管理员无法执行此操作"))
-	//		return
-	//	}
-	//
-	//	sourceList, err = model.GetSourcesByUserID(channelChat.ID)
-	//	if err != nil {
-	//		zap.S().Errorf(err.Error())
-	//		ctx.Send("导出失败")
-	//		return
-	//	}
-	//}
-	//
-	//if len(sourceList) == 0 {
-	//	ctx.Send("订阅列表为空")
-	//	return
-	//}
-	//
-	//opmlStr, err := opml.ToOPML(sourceList)
-	//if err != nil {
-	//	ctx.Send("导出失败")
-	//	return
-	//}
-	//opmlFile := &tb.Document{File: tb.FromReader(strings.NewReader(opmlStr))}
-	//opmlFile.FileName = fmt.Sprintf("subscriptions_%d.opml", time.Now().Unix())
-	//if err := ctx.Send(opmlFile); err != nil {
-	//	ctx.Send("导出失败")
-	//	zap.S().Errorf("send opml file failed, err:%+v", err)
-	//}
+func (e *Export) Description() string {
+	return "导出opml"
+}
+
+func (e *Export) Command() string {
+	return "/export"
+}
+
+func (e *Export) getChatSources(id int64) ([]model.Source, error) {
+	sources, err := model.GetSourcesByUserID(id)
+	if err != nil {
+		return nil, err
+	}
+	return sources, nil
+}
+
+func (e *Export) getChannelSources(bot *tb.Bot, opUserID int64, channelName string) ([]model.Source, error) {
+	// 导出channel订阅
+	channelChat, err := bot.ChatByUsername(channelName)
+	if err != nil {
+		return nil, errors.New("无法获取频道信息")
+	}
+
+	adminList, err := bot.AdminsOf(channelChat)
+	if err != nil {
+		return nil, errors.New("无法获取频道管理员信息")
+	}
+
+	senderIsAdmin := false
+	for _, admin := range adminList {
+		if opUserID == admin.User.ID {
+			senderIsAdmin = true
+			break
+		}
+	}
+
+	if !senderIsAdmin {
+		return nil, errors.New("非频道管理员无法执行此操作")
+	}
+
+	sources, err := e.getChatSources(channelChat.ID)
+	if err != nil {
+		zap.S().Error(err)
+		return nil, errors.New("获取订阅源信息失败")
+	}
+	return sources, nil
+}
+
+func (e *Export) Handle(ctx tb.Context) error {
+	mention := message.MentionFromMessage(ctx.Message())
+	var sourceList []model.Source
+	if mention == "" {
+		var err error
+		sourceList, err = e.getChatSources(ctx.Chat().ID)
+		if err != nil {
+			zap.S().Warnf(err.Error())
+			return ctx.Send("导出失败")
+		}
+	} else {
+		var err error
+		sourceList, err = e.getChannelSources(ctx.Bot(), ctx.Chat().ID, mention)
+		if err != nil {
+			zap.S().Warnf(err.Error())
+			return ctx.Send(err.Error())
+		}
+	}
+
+	if len(sourceList) == 0 {
+		return ctx.Send("订阅列表为空")
+	}
+
+	opmlStr, err := opml.ToOPML(sourceList)
+	if err != nil {
+		return ctx.Send("导出失败")
+	}
+	opmlFile := &tb.Document{File: tb.FromReader(strings.NewReader(opmlStr))}
+	opmlFile.FileName = fmt.Sprintf("subscriptions_%d.opml", time.Now().Unix())
+	if err := ctx.Send(opmlFile); err != nil {
+		zap.S().Errorf("send opml file failed, err:%+v", err)
+		return ctx.Send("导出失败")
+	}
+	return nil
 }
