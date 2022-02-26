49f3b4a2dfecd867b91d2e3ae4bb84d2b8d2a254
indes <wl@live.it>
2022-02-26 17:13:10 +0800

    test: remove test

--- internal/bot/service_test.go
+++ internal/bot/service_test.go
@@ -1,10 +1,10 @@
 package bot
 
 import (
-	"github.com/indes/flowerss-bot/internal/config"
+	"testing"
+
 	"github.com/magiconair/properties/assert"
 	tb "gopkg.in/telebot.v3"
-	"testing"
 )
 
 // TestGetMentionFromMessage test GetMentionFromMessage
@@ -14,13 +14,15 @@ func TestGetMentionFromMessage(t *testing.T) {
 		m           *tb.Message
 		wantMention string
 	}{
-		{"null mention",
+		{
+			"null mention",
 			&tb.Message{
 				Text: "hello world",
 			},
 			"",
 		},
-		{"one text mention",
+		{
+			"one text mention",
 			&tb.Message{
 				Text: "@hello world",
 				Entities: []tb.MessageEntity{
@@ -33,7 +35,8 @@ func TestGetMentionFromMessage(t *testing.T) {
 			},
 			"@hello",
 		},
-		{"multiple text mention", // get first mention
+		{
+			"multiple text mention", // get first mention
 			&tb.Message{
 				Text: "@hello @world!",
 				Entities: []tb.MessageEntity{
@@ -51,7 +54,8 @@ func TestGetMentionFromMessage(t *testing.T) {
 			},
 			"@hello",
 		},
-		{"one caption mention",
+		{
+			"one caption mention",
 			&tb.Message{
 				Caption: "@hello world",
 				CaptionEntities: []tb.MessageEntity{
@@ -64,7 +68,8 @@ func TestGetMentionFromMessage(t *testing.T) {
 			},
 			"@hello",
 		},
-		{"multiple caption mention", // get first mention
+		{
+			"multiple caption mention", // get first mention
 			&tb.Message{
 				Caption: "@hello @world!",
 				CaptionEntities: []tb.MessageEntity{
@@ -85,84 +90,11 @@ func TestGetMentionFromMessage(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotMention := GetMentionFromMessage(tt.m)
-			assert.Equal(t, gotMention, tt.wantMention)
-		})
-	}
-}
-
-// Test_isUserAllowed test isUserAllowed
-func Test_isUserAllowed(t *testing.T) {
-	tests := []struct {
-		name string
-		upd  *tb.Update
-		want bool
-	}{
-		{
-			"错误消息1",
-			nil,
-			false,
-		},
-		{
-			"错误消息2",
-			&tb.Update{},
-			false,
-		},
-		{
-			"anybody",
-			&tb.Update{
-				Message: &tb.Message{
-					Sender: &tb.User{
-						ID: 123,
-					},
-				},
+		t.Run(
+			tt.name, func(t *testing.T) {
+				gotMention := GetMentionFromMessage(tt.m)
+				assert.Equal(t, gotMention, tt.wantMention)
 			},
-			true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := isUserAllowed(tt.upd)
-			assert.Equal(t, got, tt.want)
-		})
-	}
-
-	config.AllowUsers = append(config.AllowUsers, 123)
-
-	tests = []struct {
-		name string
-		upd  *tb.Update
-		want bool
-	}{
-		{
-			"白名单用户",
-			&tb.Update{
-				Message: &tb.Message{
-					Sender: &tb.User{
-						ID: 123,
-					},
-				},
-			},
-			true,
-		},
-		{
-			"非白名单用户",
-			&tb.Update{
-				Message: &tb.Message{
-					Sender: &tb.User{
-						ID: 321,
-					},
-				},
-			},
-			false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := isUserAllowed(tt.upd)
-			assert.Equal(t, got, tt.want)
-		})
+		)
 	}
 }
