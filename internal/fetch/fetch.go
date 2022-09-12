4b0e339f32a297aaa759facf48a3d42fa619264f
hellodword <46193371+hellodword@users.noreply.github.com>
2022-09-12 09:31:58 +0800

    fix: error with gbk encoding

--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -3,10 +3,7 @@ package fetch
 import (
 	"github.com/SlyMarbo/rss"
 	"github.com/indes/flowerss-bot/pkg/client"
-	"io"
 	"net/http"
-	"strings"
-	"unicode"
 )
 
 // FetchFunc rss fetch func
@@ -16,19 +13,6 @@ func FetchFunc(client *client.HttpClient) rss.FetchFunc {
 		if err != nil {
 			return nil, err
 		}
-		defer resp.Body.Close()
-
-		var data []byte
-		if data, err = io.ReadAll(resp.Body); err != nil {
-			return nil, err
-		}
-
-		resp.Body = io.NopCloser(strings.NewReader(strings.Map(func(r rune) rune {
-			if unicode.IsPrint(r) {
-				return r
-			}
-			return -1
-		}, string(data))))
 		return
 	}
 }
